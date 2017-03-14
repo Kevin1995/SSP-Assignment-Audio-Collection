11f27b3bf76e423e6fca59daadd00c258395843c
Kevin1995 <kevinkehoe18@gmail.com>
2017-03-14 11:54:20 +0000

    Setting up SQL

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -24,6 +26,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+var expressSessionOptions = {
+  secret:'mySecret',
+  resave: false,
+  saveUninitialized: false
+}
+app.use(session(expressSessionOptions));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
+
+var dbConnectionInfo = {
+  host : 'eu-cdbr-azure-west-d.cloudapp.net',
+  user : 'b921d0b7f353bc',
+  password : 'd05fc196',
+  database : 'audio_collections'
+};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
