2b38937c9e185f58b6aff40a5dc720078db3ebfb
Kevin1995 <kevinkehoe18@gmail.com>
2017-03-15 19:24:10 +0000

    adding navigation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/login', login);
 
 var expressSessionOptions = {
   secret:'mySecret',
--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,25 @@ var dbConnectionInfo = {
   database : 'audio_collections'
 };
 
+router.get('/playlists', function(req, res, next) {
+  res.render('playlists', { title: 'Playlists' });  
+});
+
+router.get('/new_playlist', function(req, res, next) {
+  res.render('add_playlist', { title: 'Add new Playlist' });  
+});
+
+router.get('/name_of_playlist', function(req, res, next) {
+  res.render('playlist_name', { title: 'playlist_name' });  
+});
+
+router.get('/add_audio_file', function(req, res, next) {
+  res.render('add_audio', { title: 'Add Audio Page' });  
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
