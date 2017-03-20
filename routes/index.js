2555ee91b8380965c04c3829604d8c35fa1129af
Kevin1995 <kevinkehoe18@gmail.com>
2017-03-20 19:17:37 +0000

    update
    Can now make playlists and songs just need soundcloud links

--- routes/index.js
+++ routes/index.js
@@ -49,11 +49,10 @@ router.get('/', function(req, res, next) {
 
     var allPlaylists = new Array();
 
-    for (var i=0; i<results.length; i++) {
+    for (var i = 0; i < results.length; i++) {
       var playlist = {};
       playlist.id = results[i].id;
       playlist.text = results[i].text;
-      playlist.date = new Date(results[i].date);
 
       console.log(JSON.stringify(playlist));
 
@@ -65,6 +64,43 @@ router.get('/', function(req, res, next) {
     res.render('playlists', {playlists: allPlaylists});
   });
 
+  router.get('/users/playlistCreated', function(req, res, next) {
+  var dbConnection = mysql.createConnection(dbConnectionInfo);
+  dbConnection.connect();
+
+  dbConnection.on('error', function(err) {
+    if (err.code == 'PROTOCOL_SEQUENCE_TIMEOUT') {
+      // Let's just ignore this
+      console.log('Got a DB PROTOCOL_SEQUENCE_TIMEOUT Error ... ignoring ');
+    } else {
+      // I really should do something better here
+      console.log('Got a DB Error: ', err);
+    }
+  });
+
+  dbConnection.query('SELECT * FROM Songs', function(err, results, fields){
+    if (err) {
+      throw err;
+    }
+
+    var allSongs = new Array();
+
+    for (var i = 0; i < results.length; i++) {
+      var song = {};
+      song.id = results[i].id;
+      song.text = results[i].text;
+
+      console.log(JSON.stringify(song));
+
+      allSongs.push(song);
+    }
+   
+    dbConnection.end();
+
+    res.render('name_of_created_playlist', {songs: allSongs});
+  });
+
+});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,18 @@ router.get('/createPlaylist', function(req, res, next) {
   res.render('new_playlist');
 });
 
+router.get('/playlistCreated', function(req, res, next) {
+  res.render('name_of_created_playlist');
+});
+
+router.get('/songCreated', function(req, res, next) {
+  res.render('song_page');
+});
+
+router.get('/newSong', function(req, res, next) {
+  res.render('new_song');
+});
+
 router.post('/newPlaylist', function(req, res, next) {
 
   var dbConnection = mysql.createConnection(dbConnectionInfo);
@@ -29,11 +41,9 @@ router.post('/newPlaylist', function(req, res, next) {
   });
 
   var playlist = {};
-  playlist.date = new Date();
   playlist.text = req.body.thePlaylist;
 
-  var mysqlDate = playlist.date.toISOString().slice(0, 19).replace('T', ' ');
-  dbConnection.query('INSERT INTO Playlists (playlist_name, playlist_date) VALUES(?,?)',[playlist.text, mysqlDate], function(err, results,fields) {
+  dbConnection.query('INSERT INTO Playlists (playlist_name) VALUES(?)',[playlist.text], function(err, results,fields) {
     // error will be an Error if one occurred during the query
     // results will contain the results of the query
     // fields will contain information about the returned results fields (if any)
@@ -53,6 +63,47 @@ router.post('/newPlaylist', function(req, res, next) {
 
     res.redirect('/');
   });
+
+  router.post('/newSongAdded', function(req, res, next) {
+
+  var dbConnection = mysql.createConnection(dbConnectionInfo);
+  dbConnection.connect();
+
+  dbConnection.on('error', function(err) {
+    if (err.code == 'PROTOCOL_SEQUENCE_TIMEOUT') {
+      // Let's just ignore this
+      console.log('Got a DB PROTOCOL_SEQUENCE_TIMEOUT Error ... ignoring ');
+    } else {
+      // I really should do something better here
+      console.log('Got a DB Error: ', err);
+    }
+  });
+
+  var song = {};
+  song.text = req.body.theSong;
+
+  dbConnection.query('INSERT INTO Songs (song_name) VALUES(?)',[song.text], function(err, results,fields) {
+    // error will be an Error if one occurred during the query
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
+    if (err) {
+      throw err;
+    }
+
+    // notice that results.insertId will give you the value of the AI (auto-increment) field
+    song.id = results.insertId;
+
+    // Going to convert my joke object to a JSON string a print it out to the console
+    console.log(JSON.stringify(song));
+
+    // Close the connection and make sure you do it BEFORE you redirect
+    dbConnection.end();
+
+
+    res.redirect('/');
+  });
+
+});
 });
 
 module.exports = router;
